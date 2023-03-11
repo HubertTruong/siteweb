cbfc4095344edb2617ae663932a6a9e6cac1d96b
Archi398 <71143565+Archi398@users.noreply.github.com>
2023-03-11 17:11:09 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# exécuter la commande docker-compose build
+docker-compose build
+
+# vérifier si la commande a échoué
+if [ $? -ne 0 ]; then
+  echo "La commande docker-compose build a échoué" | mail -s "Erreur de construction de Docker Compose" archisabatier@gmail.com
+  else 
+  docker-compose up -d 
+fi
